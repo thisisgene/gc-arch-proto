441b108190a0f57a649f41786f70059b190fb018
thisisgene <emdo2000@gmail.com>
2018-05-03 19:47:14 +0200

    Initial commit.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//   var $grid = $('.grid').packery({
+//     itemSelector: '.grid-item',
+//     // columnWidth helps with drop positioning
+//     columnWidth: 0
+//   });
+//
+// // make all grid-items draggable
+//   $grid.find('.grid-item').each( function( i, gridItem ) {
+//     var draggie = new Draggabilly( gridItem );
+//     // bind drag events to Packery
+//     $grid.packery( 'bindDraggabillyEvents', draggie );
+//   });
+//
+// });
+
